5a2a417f172bb5f659b3fcc84f59a9984a351253
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-03-08 19:17:34 +0300

    Created test for the ChangeTaskTitle, created ChangeTaskTitleAC

--- src/state/task-reducer.test.ts
+++ src/state/task-reducer.test.ts
@@ -1,5 +1,5 @@
 import {TasksStateType} from "../App";
-import {AddAC, ChangeTaskStatusAC, RemoveAC, TaskReducer} from "./task-reducer";
+import {AddAC, ChangeTaskStatusAC, ChangeTaskTitleAC, RemoveAC, TaskReducer} from "./task-reducer";
 
 test('Should remove task', () => {
 
@@ -92,4 +92,27 @@ test('Status of specified task should be changed', () => {
     expect(endState['todolistID2'].length).toBe(3)
     expect(endState['todolistID2'][1].isDone).toBe(false)
     expect(endState['todolistID1'][1].isDone).toBe(true)
+})
+
+
+
+test('Title should be changed', () => {
+    const startState: TasksStateType = {
+        'todolistID1': [
+            {id: '1', title: "HTML & CSS", isDone: true},
+            {id: '2', title: "JS", isDone: true},
+            {id: '3', title: "React", isDone: false},
+        ],
+        'todolistID2': [
+            {id: '1', title: "Auto", isDone: false},
+            {id: '2', title: "Scissors", isDone: true},
+            {id: '3', title: "Mobile Phone", isDone: false},
+        ],
+    }
+
+    const endState = TaskReducer(startState, ChangeTaskTitleAC('2', 'iPad Air', 'todolistID2'))
+
+    expect(endState['todolistID2'].length).toBe(3)
+    expect(endState['todolistID2'][1].title).toBe('iPad Air')
+    expect(endState['todolistID1'][1].title).toBe("JS")
 })
--- src/state/task-reducer.ts
+++ src/state/task-reducer.ts
@@ -3,7 +3,9 @@ import {TasksStateType} from "../App";
 const REMOVE_TASK = 'REMOVE-TASK'
 const ADD_TASK = 'ADD-TASK'
 const CHANGE_TASK_STATUS = 'CHANGE-TASK-STATUS'
-type TaskReducerActionType = RemoveACType | AddACType | ChangeTaskStatusACType
+const CHANGE_TASK_TITLE = 'CHANGE-TASK-TITLE'
+
+type TaskReducerActionType = RemoveACType | AddACType | ChangeTaskStatusACType | ChangeTaskTitleACType
 
 
 export const TaskReducer = (state: TasksStateType, action: TaskReducerActionType) => {
@@ -32,6 +34,16 @@ export const TaskReducer = (state: TasksStateType, action: TaskReducerActionType
                     )
             }
         }
+        case CHANGE_TASK_TITLE: {
+            return {
+                ...state,
+                [action.todolistID]: state[action.todolistID]
+                    .map(el => el.id === action.taskId?
+                        {...el, title: action.newTitle}:
+                        el
+                    )
+            }
+        }
         default:
             throw new Error("I don't understand this type")
     }
@@ -46,3 +58,6 @@ export const AddAC = (todolistID1: string, newTitle: string) => ({type: ADD_TASK
 
 type ChangeTaskStatusACType = ReturnType<typeof ChangeTaskStatusAC>
 export const ChangeTaskStatusAC = (taskId: string, isDone: boolean, todolistID: string) => ({type: CHANGE_TASK_STATUS,taskId, isDone, todolistID}as const)
+
+type ChangeTaskTitleACType = ReturnType<typeof ChangeTaskTitleAC>
+export const ChangeTaskTitleAC = (taskId: string, newTitle: string, todolistID: string) => ({type: CHANGE_TASK_TITLE,taskId, newTitle, todolistID}as const)
