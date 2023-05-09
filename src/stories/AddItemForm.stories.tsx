25ee1511f54af19f986ee082c7032e16fd25d212
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-05-09 17:35:32 +0300

    Added AddItemFormWithErrorStory

---
+++ src/stories/AddItemForm.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import {AddItemForm, AddItemFormType} from '../components/AddItemForm';
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import {IconButton, TextField} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+const meta: Meta<typeof AddItemForm> = {
+  title: 'TODOLIST/AddItemForm',
+  component: AddItemForm,
+  tags: ['autodocs'],
+  argTypes: {
+    addTask: {
+      description: 'Button clicked inside form',
+    }
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof AddItemForm>;
+
+export const AddItemFormStory: Story = {
+  args: {
+    addTask: action('Button clicked inside form')
+  }
+};
+export const AddItemFormWithErrorStory = (args: AddItemFormType) => {
+  const [value, setValue] = useState('')
+  const [error, setError] = useState<string | null>('Title is required')
+  const inputOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value)
+  }
+  const onKeyDownInput = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onClickAdd()
+    }
+  }
+  const onClickAdd = () => {
+    if (value.trim() !== '') {
+      args.addTask(value)
+      setValue('')
+      setError(null)
+    } else {
+      setValue('')
+      setError('Title is required')
+    }
+  }
+  return (
+      <div>
+        <TextField variant="outlined"
+                   error={!!error}
+                   size='small'
+                   label="Title"
+                   value={value}
+                   onChange={inputOnChangeHandler}
+                   onKeyDown={onKeyDownInput}
+        />
+        <IconButton
+            color='primary'
+            onClick={onClickAdd}>
+          <AddIcon/>
+        </IconButton>
+        {error && <div
+            style={{color: 'red'}}
+        >{error}</div>}
+      </div>
+  )
+};
+
+
+
+
