eb5a4f5442ece988162701874e1aec4f01efc424
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-07-28 07:14:46 +0300

    Added TS for the response

--- src/api/task-api.ts
+++ src/api/task-api.ts
@@ -5,24 +5,46 @@ const instance = axios.create({
     withCredentials: true
 })
 
+type TaskType = {
+    id: string
+    title: string
+    description: null | string
+    todoListId: string
+    order: number
+    status: number
+    priority: number
+    startDate: null | Date
+    deadline: null | string
+    addedDate: Date
+}
+type GetTaskResponseType = {
+    error: null
+    items: TaskType[]
+    totalCount: number
+}
+type ResponseTaskType<T = {}> = {
+    data: T
+    messages: string[]
+    fieldsErrors: string[]
+    resultCode: number
+}
+
 
 export const taskApi = {
     getTasks(todolistId: string) {
         return instance
-            .get(`/todo-lists/${todolistId}/tasks`)
+            .get<GetTaskResponseType>(`/todo-lists/${todolistId}/tasks`)
     },
     createTask(todolistId: string, title: string) {
         return instance
-            .post(`/todo-lists/${todolistId}/tasks`, {title})
+            .post<ResponseTaskType<{ item: TaskType }>>(`/todo-lists/${todolistId}/tasks`, {title})
     },
     deleteTask(todolistId: string, taskId: string) {
         return instance
-            .delete(`/todo-lists/${todolistId}/tasks/${taskId}`)
+            .delete<ResponseTaskType>(`/todo-lists/${todolistId}/tasks/${taskId}`)
     },
     updateTaskTitle(todolistId: string, taskId: string, title: string) {
         return instance
-            .put(`/todo-lists/${todolistId}/tasks/${taskId}`, {title})
-    },
-
-
+            .put<ResponseTaskType<{ item: TaskType }>>(`/todo-lists/${todolistId}/tasks/${taskId}`, {title})
+    }
 }
