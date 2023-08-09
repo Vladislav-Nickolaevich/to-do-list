aac15cb3e37eddbb25a4bdcbb6e0079cd0b98025
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-08-09 07:59:10 +0300

    Added new types enum

--- src/api/task-api.ts
+++ src/api/task-api.ts
@@ -5,17 +5,31 @@ const instance = axios.create({
     withCredentials: true
 })
 
-type TaskType = {
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    Hi = 2,
+    Urgently = 3,
+    Later = 4
+}
+
+export type TaskType = {
     id: string
     title: string
     description: null | string
     todoListId: string
     order: number
-    status: number
-    priority: number
-    startDate: null | Date
+    status: TaskStatuses
+    priority: TaskPriorities
+    startDate: string | Date | null
     deadline: null | string
-    addedDate: Date
+    addedDate: Date | string
 }
 type GetTaskResponseType = {
     error: null
