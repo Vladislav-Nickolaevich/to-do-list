9ae564de113234e220896a7a7b5380b423092a5a
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-09-12 15:23:37 +0300

    Error Snackbar

---
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,30 @@
+import React  from 'react'
+import Snackbar from '@mui/material/Snackbar'
+import MuiAlert, { AlertProps } from '@mui/material/Alert'
+import {setErrorAC} from "../../app/app-reducer";
+import {useAppDispatch, useAppSelector} from "../../state/store";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
+})
+
+export function ErrorSnackbar() {
+    const messageError = useAppSelector<null | string>(state => state.app.error)
+
+
+    const dispatch = useAppDispatch()
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return
+        }
+        dispatch(setErrorAC(null))
+    }
+    return (
+        <Snackbar open={!!messageError} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity='error' sx={{width: '100%'}}>
+                {messageError}
+            </Alert>
+        </Snackbar>
+    )
+}
