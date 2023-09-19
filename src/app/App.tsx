f7eb70a3a7077386ef27012ab536a0b8c9cb4011
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-09-19 16:36:53 +0300

    Added useEffect, logOut on the button, and check initialized

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {AppBar, Button, Container, IconButton, Toolbar, Typography} from "@mui/material";
+import {AppBar, Button, CircularProgress, Container, IconButton, Toolbar, Typography} from "@mui/material";
 import {Menu} from "@mui/icons-material";
 import LinearProgress from "@mui/material/LinearProgress"
-import {useAppSelector} from "./store";
+import {useAppDispatch, useAppSelector} from "./store";
 import {TodolistsList} from "../features/TodolistList";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
 import {Login} from "../features/Login/Login";
 import {Route, Routes, Navigate} from "react-router-dom";
+import {logOutTC, meTC} from "../features/Login/auth-reducer";
 
 
 function App() {
     const status = useAppSelector<RequestStatusType>(state => state.app.status)
+    const dispatch = useAppDispatch()
+    const initialized = useAppSelector<boolean>(state => state.app.initialized)
+    const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
 
+    useEffect(() => {
+        dispatch(meTC())
+    }, [])
+
+    if(!initialized){
+        return (
+            <div style={{top: '30%', width: '100%', position: 'fixed', textAlign: 'center'}}>
+                <CircularProgress/>
+            </div>
+            )
+    }
+    const onLogOut = () => {
+        dispatch(logOutTC())
+    }
     return (
         <div className="App">
             <ErrorSnackbar/>
@@ -31,7 +49,7 @@ function App() {
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                         TodoLists
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    {isLoggedIn && <Button color="inherit" onClick={onLogOut}>Log Out</Button>}
                 </Toolbar>
                 {status === 'loading' &&
                     <div style={{marginTop: '60px', width: '100%', position: 'fixed'}}>
